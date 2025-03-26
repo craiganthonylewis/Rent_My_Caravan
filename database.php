22278d07bbaf4205b48ba4ddb680db9718a3903a
craiganthonylewis <craiganthonylewis@outlook.com>
2025-03-26 11:24:57 +0000

    Responsive 960 Grid Base
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

---
+++ database.php
@@ -0,0 +1,12 @@
+<!-- Coded by Craig Lewis -->
+<?php
+ $db_host = "localhost";
+ $db_user = "root";
+ $db_pass = "";
+ $db_name = "rent_my_caravan";
+ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+ if (!$conn) {
+
+ }
+
+

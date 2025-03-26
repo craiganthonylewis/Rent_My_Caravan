c5ea7dd9662d5d83ebcf2f1fc833990af56442ac
craiganthonylewis <craiganthonylewis@outlook.com>
2025-03-26 11:33:34 +0000

    Responsive 960 Grid Base
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

--- database.php
+++ database.php
@@ -1,11 +1,15 @@
 <!-- Coded by Craig Lewis -->
 <?php
- $db_host = "localhost";
+
+ $db_host = "localhost"; #Holds names of the server
  $db_user = "root";
- $db_pass = "";
- $db_name = "rent_my_caravan";
- $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
- if (!$conn) {
+ $db_pass = ""; #Password
+ $db_name = "rent_my_caravan"; #Name of database
+
+ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name); #Declares connection variables
+
+#Verifies connection
+if (!$conn->connect_error) {
 
  }
 

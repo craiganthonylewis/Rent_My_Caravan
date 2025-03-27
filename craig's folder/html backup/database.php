429855fe974c30fb520459dc39f30b6e763e072a
craiganthonylewis <craiganthonylewis@outlook.com>
2025-03-27 18:16:05 +0000

    Responsive 960 Grid Base
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

---
+++ craig's folder/html backup/database.php
@@ -0,0 +1,22 @@
+<!-- Database connection to rentmycar-->
+<!-- Coded by Craig Lewis -->
+
+<?php
+
+ $db_server = "localhost"; #Holds names of the server
+ $db_user = "root"; #MySQL database user //root = default and a security risk
+ $db_pass = ""; #Password
+ $db_name = "rentmycar"; #Name of database
+
+ $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name); #Declares connection variables
+
+#Verifies connection
+if ($conn) {
+    echo("You are connected to the database!" . mysqli_connect_error());
+} else {
+    echo "Could not connect to database!";
+}
+
+?>
+
+
---
+++ craig's folder/html backup/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!-- Coded by Craig Lewis -->
+<html lang="en">
+
+<head>
+
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/media_queries.css">
+  <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/font.css">
+
+  <meta charset="UTF-8">
+  <title>Rent My Caravan</title>
+</head>
+
+<body>
+<?php include('header.php');?>
+<?php include('home_navigation.php');?>
+<main>
+    <section>
+
+        <div class = "container_960">
+            <div class = "column_12" id = "background_search">
+
+
+
+                <div class="column_6" id="title_container">
+
+                    <ul>
+                        <li id ="title_1">Rent my Caravan</li>
+                        <li id ="title_2">Holiday for less!</li>
+                    </ul>
+                </div>
+
+                <div class="column_6" id="signup_button_container">
+                <button>Sign up for free</button>
+                </div>
+
+                <div class="column_12" id="search_bar_container">
+                        <div id = "search_bar">
+                            <label>
+                                <input type="search" placeholder="Search here...">
+                            </label>
+                        </div>
+
+                    <button id = "search_button"></button>
+
+                </div>
+
+
+            </div>
+
+                </div>
+
+
+    </section>
+</main>
+<?php include('footer.php');?>
+</body>
+</html>
+

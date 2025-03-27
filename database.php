a2a100a0043e38f81e68c778f842ffff41e789fc
craiganthonylewis <craiganthonylewis@outlook.com>
2025-03-27 14:05:44 +0000

    Responsive 960 Grid Base
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

--- database.php
+++ database.php
@@ -6,15 +6,15 @@
  $db_server = "localhost"; #Holds names of the server
  $db_user = "root"; #MySQL database user //root = default and a security risk
  $db_pass = ""; #Password
- $db_name = "rent_my_caravan"; #Name of database
+ $db_name = "rentmycar"; #Name of database
 
  $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name); #Declares connection variables
 
 #Verifies connection
 if ($conn) {
-    echo("Connection failed" . mysqli_connect_error());
+    echo("You are connected to the database!" . mysqli_connect_error());
 } else {
-    echo "Connection successful";
+    echo "Could not connect to database!";
 }
 
 ?>

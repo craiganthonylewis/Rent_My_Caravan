73eef8f5a0658bec42a49c583ecfa1dccb3dc66e
mrxl2233 <yurij.filin.03@gmail.com>
2025-03-27 09:50:50 +0000

    .

--- database/users.php
+++ database/users.php
@@ -1,4 +1,22 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=users';
-$username = 'root';
+
+ $db_server = "localhost"; 
+ $db_user = "root"; 
+ $db_pass = "";
+ $db_name = "rent_my_caravan"; 
+
+ try{
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+ }
+
+ catch(mysqli_sql_exception){
+    echo "No connection";
+ }
+ if($conn){
+     echo "You are connected: "
+ }
+ else{
+    echo "No connection";
+}
+
 ?>

3c204586a9ae8f8a0b70bbe3240861586efc2b34
mrxl2233 <yurij.filin.03@gmail.com>
2025-03-27 12:59:26 +0000

    .

--- database/users.php
+++ database/users.php
@@ -1,22 +1,10 @@
 <?php
+// Create connection
+$con=mysqli_connect(host,username,password,dbname); 
 
- $db_server = "localhost"; 
- $db_user = "root"; 
- $db_pass = "";
- $db_name = "rent_my_caravan"; 
-
- try{
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
- }
-
- catch(mysqli_sql_exception){
-    echo "No connection";
- }
- if($conn){
-     echo "You are connected: "
- }
- else{
-    echo "No connection";
-}
-
-?>
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?> 

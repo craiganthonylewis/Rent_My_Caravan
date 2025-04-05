2b075bbe20492607197fc4e755a8eb4f7d2a75c5
craiganthonylewis <craiganthonylewis@outlook.com>
2025-04-05 20:09:21 +0100

    Index/HomePage, Header, Nav and Footer
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

--- navigation.php
+++ navigation.php
@@ -16,26 +16,26 @@
     <nav>
         <div class="container_960">
             <div class="column_12" id="navigation_container">
-                <div class="column_6" id="search_bar_nav">
+                <div class="column_5" id="search_bar_nav">
                     <div id="search_bar_nav_icon"></div>
                     <label>
                         <input type="search" placeholder="Search here...">
                     </label>
                 </div>
+
                 <button id="search_button"></button>
+
+                <div class = "column_5" id = "nav_links">
+                    <ul>
+                        <li class = "nav_link" id = "home_link"><a href="index.php">home</a></li>
+                        <li class = "nav_link" id = "about_link"><a href="index.php">caravans</a></li>
+                        <li class = "nav_link" id = "contact_link"><a href="index.php">about</a></li>
+                        <li class = "nav_link" id = "services_link"><a href="index.php">contact us</a></li>
+                    </ul>
+                </div>
+
             </div>
         </div>
-
-
-                <div class = "column_6" id = "nav_links">
-                <ul>
-                    <li class = "nav_link" id = "home_link"><a href="index.php">home</a></li>
-                    <li class = "nav_link" id = "about_link"><a href="index.php">caravans</a></li>
-                    <li class = "nav_link" id = "contact_link"><a href="index.php">about</a></li>
-                    <li class = "nav_link" id = "services_link"><a href="index.php">contact us</a></li>
-                </ul>
-            </div>
-
 
     </nav>
 

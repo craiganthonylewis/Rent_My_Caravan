dd9af08cb1a8acaf21bf5ccccd04edd0c2ab27c4
craiganthonylewis <craiganthonylewis@outlook.com>
2025-04-10 12:29:39 +0100

    Index/HomePage, Header, Nav and Footer
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

--- js/title_animation.js
+++ js/title_animation.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
 
     $("#title_1").fadeOut("slow");
     $("#title_1").fadeIn("slow");
+
+    $("#title_1").animate({
+        height: '60px',
+        opacity: '0.4',
+    });
+
     $("#title_2").fadeOut("slow");
     $("#title_2").fadeIn("slow");
 });

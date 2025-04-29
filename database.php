c93c03d392016604aded484648a5e0c88b46312a
soapiest-bubble <st20261632@outlook.cardiffmet.ac.uk>
2025-04-29 22:54:46 +0100

    database connect

--- database.php
+++ database.php
@@ -6,7 +6,7 @@
  $db_server = "localhost"; #Holds names of the server
  $db_user = "root"; #MySQL database user //root = default and a security risk
  $db_pass = ""; #Password
- $db_name = "rentmycar"; #Name of database
+ $db_name = "rentmycaravan_db"; #Name of database
 
  $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name); #Declares connection variables
 

7b8dd4381060427ac082a69becf890b2f404c14b
craiganthonylewis <craiganthonylewis@outlook.com>
2025-05-01 23:05:00 +0100

    login backend Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>
    Signed-off-by: craiganthonylewis <craiganthonylewis@outlook.com>

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_Entry = $_POST["email"];
     $password_Entry = $_POST["password"];
 
-    echo $email_Entry . " " . $password_Entry;
+
 
     $conn = new mysqli("localhost", "root", "", "rentmycaravan_db");
     $statement = $conn->prepare("SELECT `user_id`, `username`, `password` FROM users WHERE username=?");
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $statement->execute();
     $statement->store_result();
     if ($statement->num_rows == 1) {
-        echo "one user found";
+
         $statement->bind_result($id, $username, $password);
         $statement->fetch();
         $_SESSION["user_id"] = $id;

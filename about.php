980530bc3cad84f87c98ea0ba4f2ae2a7790aee2
soapiest-bubble <st20261632@outlook.cardiffmet.ac.uk>
2025-05-03 22:31:07 +0100

    playing with divs - detail view

--- about.php
+++ about.php
@@ -31,15 +31,42 @@
         <section>
             <div class = "container_960">
                 <div class = "column_12" id="container_background">
-                <div class="column_12" id="container_title"><h1>About</h1></div>
+                    <div class="column_12" id="container_title"><h1>About</h1></div>
+                    
+                    <div class = "column_10" id = "text_container">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
 
+                    <div class="column_12" id="container_title"><h1>Creators</h1></div>
 
+                    <div class="column_2" id="text_container">
+                        <p>Person 1</p>
+                    </div>
+
+                    <div class="column_2" id="text_container">
+                        <p>Person 2</p>
+                    </div>
+
+                    <div class="column_2" id="text_container">
+                        <p>Person 3</p>
+                    </div>
+
+                    <div class="column_2" id="text_container">
+                        <p >Person 4</p>
+                    </div>
                 </div>
             </div>
         </section>
     </main>
-
+    <?php include('footer.php');?>
 </body>
-
-
 </html>
--- caravan_detail_view.php
+++ caravan_detail_view.php
@@ -33,8 +33,25 @@ include('navigation.php');// always show same nav
 <main>
     <section>
         <div class = "container_960">
-            <div class = "column_12" id="container_background">
-                detail view
+            <div class = "column_12" id = "container_background">
+                <div class = "column_12" id = "container_title"><h1>Caravans</h1></div>
+
+                <div class = "column_4" id = "text_container">
+                    <p>Image placeholder</p>
+                </div>
+
+                <div class = "column_6">
+                    <div class = "column_6" id = "container_title"><h1>Caravan Title</h1></div>
+                    <div class = "column_6" id = "text_container">
+                        <p>Caravan description placeholder</p>
+                    </div>
+                    <div class = "column_6">
+                        <div class = "column_2" id = "text_container">
+                            <p>Price</p>
+                        </div>
+                        <div class = "column_4"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>

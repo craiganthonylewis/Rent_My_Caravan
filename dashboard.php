9bd5780a7c0276c1c426d4252ef3d96229eca55e
soapiest-bubble <st20261632@outlook.cardiffmet.ac.uk>
2025-05-06 13:39:18 +0100

    dashboard added

---
+++ dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!-- Coded by Ezme Clark ST20261632-->
+<html lang="en">
+
+<?php 
+// by Ezme Clark ST20261632
+session_start();
+//required. shoes different header if logged in
+include('header_handler.php');
+// always show same nav
+include('home_navigation.php');
+?>
+
+<head>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/media_queries.css">
+    <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="css/font.css">
+
+    <!--JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!-- Javascript -->
+    <script src="js/title_animation.js"></script>
+
+    <meta charset="UTF-8">
+    <!-- Put username in tab title -->
+    <title><?php 
+    $username = isset($_SESSION['username'])?$_SESSION['username'] : 'User'; // Catch blank username
+    echo $username;  
+    ?> | RMC</title>
+</head>
+
+<body>
+<main>
+    <section>
+        <div class = "container_960">
+            <div class = "column_6" id = "container_background">
+                <div class = "column_12" id = "container_title">
+                    <h1>Hello, <?php echo $username ?></h1>
+                </div>
+                <div class = "column_12" id = "input_title_container"> 
+                    <div class = "column_6" id = "red_button">
+                        <button id= "red_button" onclick="session_delete.php">Log out</button>
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+<?php include('footer.php') ?>
--- header_handler.php
+++ header_handler.php
@@ -4,7 +4,8 @@
 //testing
 $temp = (strlen(isset($_SESSION["user_id"]))) ;
 echo $temp;
-echo "username:  " . isset($_SESSION["username"]) . "<br>";
+$username = isset($_SESSION['username'])?$_SESSION['username'] : 'User';
+echo "username:  " . $username . "<br>";
 
 //required. shows different header if logged in
 if (strlen(isset($_SESSION["user_id"])) >= 1 ){

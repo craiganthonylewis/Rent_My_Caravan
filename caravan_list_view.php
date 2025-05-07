86fb9125c22f023a83a1128c4736d02d81275fc0
soapiest-bubble <st20261632@outlook.cardiffmet.ac.uk>
2025-05-07 11:45:32 +0100

    list view db connection

--- caravan_list_view.php
+++ caravan_list_view.php
@@ -25,6 +25,7 @@
 <body>
 <?php 
 session_start();
+include('database_connection.php'); // database connection
 include('header_handler.php'); // variable header
 include('navigation.php');// always show same nav
 ?>
@@ -46,7 +47,58 @@ include('navigation.php');// always show same nav
                 <div class="column_12"></div>
                 <!-- Create internally scrollable div that fills with items from database
                  made it a text_container type so its visible for now.-->
-                <div class="column_10" id="text_container">
+                
+                 <div class="column_10" id="text_container">
+                    <?php
+                        $caravans = $conn->prepare ("SELECT * FROM caravans");
+                        $caravans->execute();
+                        $items = $caravans -> get_result()->fetch_all(MYSQLI_ASSOC);
+                        $caravans->close();
+
+                        if ($items != NULL){
+                            foreach ($items as $item) {
+                                $title = $item['title'];
+                                $description = $item['description'];
+                                $price = $item['price'];
+                                $location = $item['location'];
+                                $caravan_id = $item['caravan_ID'];
+                                $image = $item['image_url'];
+
+                                echo "
+                                <div class = 'column_10' id = 'content_container'>
+                                    <div class = 'column_4' id = 'text_container'> 
+                                        <p>Image placeholder</p>
+                                    </div>
+                                    
+                                    <div class = 'column_8' style='margin-right:auto;'>
+                                        <div class = 'column_8'>
+                                            <div id = 'container_title' style=' margin:0;'><h1>
+                                                $title
+                                            </h1></div>
+                                        </div> 
+                                        <div class = 'column_8' >
+                                            <div class = 'column_8' id = 'text_container'>
+                                                <p>$description</p>
+                                            </div>
+                                        </div> 
+                                    </div> 
+                                    
+                                    <a href='caravan_detail_view.php?caravan_id=$caravan_id' id='green_button'>
+                                        <button type='button' id='green_button'>View</button>
+                                    </a>
+                                </div>
+                                <br>
+                                ";
+                            }
+                        } else {
+                            echo "<p> No caravans found </p>";
+                        }
+
+
+
+                    ?>
+
+
 
                 </div>
             </div>

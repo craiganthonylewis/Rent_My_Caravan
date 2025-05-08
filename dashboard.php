20538b2d0097b1402e8f64dce0a5f7d6b34a0012
mrxl2233 <yurij.filin.03@gmail.com>
2025-05-08 12:32:29 +0100

    IT IS NOT EMPTY

--- dashboard.php
+++ dashboard.php
@@ -12,28 +12,6 @@
     include('home_navigation.php');
 ?>
 
-
-<?php 
-    if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
-        $image = $_FILES['image']['tmp_name'];
-        $imgContent = file_get_contents($image);
-
-        // Insert image data into database as BLOB
-        $sql = "INSERT INTO images(image) VALUES(?)";
-        $statement = $conn->prepare($sql);
-        $statement->bind_param('s', $imgContent);
-        $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
-
-        if ($current_id) {
-            echo "Image uploaded successfully.";
-        } else {
-            echo "Image upload failed, please try again.";
-        }
-    } else {
-        echo "Please select an image file to upload.";
-    }
-?>
-
 <head>
     <!-- CSS -->
     <link rel="stylesheet" href="css/grid.css">
